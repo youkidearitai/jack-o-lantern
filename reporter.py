f673bab9bd0b26fc9ed1f7bef64a9ebf3e867c6d
youkidearitai <youkidearitai@gmail.com>
2015-10-13 21:54:06 +0900

    Count Trick-or-Treat

--- reporter.py
+++ reporter.py
@@ -5,14 +5,18 @@ import time
 
 # put device below here.
 ser = serial.Serial('/dev/tty.usbserial-AH016NS4', 9600);
+trickOrTreatCount = 0
 
 try:
     while ser.readable():
         line = ser.readline()
-        print(line)
+        if line != "":
+            trickOrTreatCount = trickOrTreatCount + 1
+            print("Trick-Or-Treat!!")
 
         time.sleep(1)
 except BaseException:
     ser.close()
     print("closed connection")
+    print("Trick-Or-Treat count: {0}".format(trickOrTreatCount))
 
