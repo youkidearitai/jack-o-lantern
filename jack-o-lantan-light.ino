c16259267e43c79f10ddec4a62a6a7a7577f2122
youkidearitai <youkidearitai@gmail.com>
2015-10-14 07:14:57 +0900

    Variables to call level

--- jack-o-lantan-light.ino
+++ jack-o-lantan-light.ino
@@ -1,33 +1,35 @@
+int trickOrTreatCallLevel = 220;
+
 int dist() {
   int i = 0;
   int analogPin = analogRead(0);
 
-  if (analogPin < 220) {
-    return 0;
+  if (analogPin < trickOrTreatCallLevel) {
+    return -1;
   }
   
-  for (i = 0;i < 20; i++) {
-    digitalWrite(11, HIGH);
-    delay(50);
-    digitalWrite(11, LOW);
-    delay(50);
-  }
-  
-  return 1;
+  return analogPin;
 }
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(11, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (dist() == 0) {
+  int distance = dist();
+  
+  if (distance == -1) {
     digitalWrite(11, HIGH);
-  } else {
-    Serial.println("1");
+  } else {  
+    for (i = 0;i < 20; i++) {
+      digitalWrite(11, HIGH);
+      delay(50);
+      digitalWrite(11, LOW);
+      delay(50);
+    }
   }
+  
+  Serial.println(distance);
   
 }
